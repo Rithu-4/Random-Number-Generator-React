35045e740db9f0d215be94cd0b70dde29379bcf1
Rithu-4 <rithurajan2000@gmail.com>
2026-04-03 20:41:00 +0530

    updated files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const [number, setNumber] = useState(null);
+
+  const generateNumber = () => {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    setNumber(randomNum);
+  };
+
+  return (
+    <div className="container">
+      <h1>🎲 Random Number Generator</h1>
+
+      {/* Conditional Rendering */}
+      <p className="result">
+        {number === null
+          ? "No number generated yet"
+          : `Generated Number: ${number}`}
+      </p>
+
+      <button onClick={generateNumber}>
+        Generate Random Number
+      </button>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
